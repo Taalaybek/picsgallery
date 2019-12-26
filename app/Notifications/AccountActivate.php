40b0021e5ffc9b0ee2dda5cd1dc462236ad95cc5
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2019-12-27 01:57:02 +0600

    User account verification notification

---
+++ app/Notifications/AccountActivate.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class AccountActivate extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = url('/api/v1/auth/account-activation/'.$notifiable->activation_token);
+        return (new MailMessage)
+                    ->subject('Confirm your account')
+                    ->line('Please click link in below for activate your account.')
+                    ->action('Confirm account', $url)
+                    ->line('If you didn\'t create account in our system so just ignore this message');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
