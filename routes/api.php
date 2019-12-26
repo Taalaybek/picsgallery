870594087e40e79064412c167d5dc995868bbd5a
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2019-12-27 02:02:59 +0600

    Registered register and activateToken api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +11,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1/auth')->group(function () {
+    Route::namespace('API\V1\Auth')->group(function () {
+        Route::post('register', 'RegisterController@register')->name('auth.register');
+        Route::get('account-activation/{token}', 'AccountActivationController@activateToken')->name('auth.account.activate');
+    });
 });
