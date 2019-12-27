0609c794fc3719c00133a3cfbda72ae7360ae687
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2019-12-27 12:17:39 +0600

    Implemented AuthController@refreshToken method

--- app/Http/Controllers/API/V1/Auth/AuthController.php
+++ app/Http/Controllers/API/V1/Auth/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\V1\Auth;
 
 use App\Http\Resources\UserResource;
+use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -17,5 +18,27 @@ class AuthController extends Controller
     public function user(): UserResource
     {
         return new UserResource(Auth::user());
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function refreshToken(Request $request)
+    {
+        $request->validate(['refresh_token' => 'required|string|min:60']);
+
+        $data = [
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $request->input('refresh_token'),
+            'client_id' => config('services.passport.client_id'),
+            'client_secret' => config('services.passport.client_secret'),
+            'scope' => '',
+        ];
+
+        // post request to oauth/token with refresh_token grant type
+        $request = Request::create(config('services.passport.endpoint'), 'POST', $data);
+
+        return app()->handle($request);
     }
 }
