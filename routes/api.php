6e7e9f60e957d567322d6e7da9c1833d92021ead
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2019-12-27 12:17:07 +0600

    Registered api route for refreshing token

--- routes/api.php
+++ routes/api.php
@@ -11,11 +11,13 @@
 |
 */
 
+/*** AUTHENTICATION ***/
 Route::prefix('v1/auth')->group(function () {
     Route::namespace('API\V1\Auth')->group(function () {
         Route::post('login', 'LoginController@login')->name('auth.login');
         Route::post('register', 'RegisterController@register')->name('auth.register');
         Route::get('account-activation/{token}', 'AccountActivationController@activateToken')->name('auth.account.activate');
+        Route::post('token_refresh', 'AuthController@refreshToken')->name('auth.token_refresh');
 
         Route::middleware(['auth:api', 'verified'])->group(function () {
            Route::get('user', 'AuthController@user')->name('auth.user');
