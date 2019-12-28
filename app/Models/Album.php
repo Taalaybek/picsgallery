f6b953c8842c88718a47e38be242a41b893c4653
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2019-12-28 20:26:47 +0600

    Album eloquent model

---
+++ app/Models/Album.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Album extends Model
+{
+    protected $table = 'albums';
+
+    protected $fillable = [
+        'name', 'description', 'creator_id'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'creator_id', 'id');
+    }
+}
