2e6196327c2e1d95b2bf981dd11313e1380ea4d9
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-07 13:52:49 +0600

    Created PhotoResource class

---
+++ app/Http/Resources/PhotoResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PhotoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'type' => 'photos',
+            'id' => $this->id,
+            'attributes' => [
+                'name' => $this->name,
+                'base_name' => $this->base_name,
+                'full_name' => $this->full_name,
+                'mime_type' => $this->mime_type,
+                'size' => $this->size,
+                'original_file_path' => $this->original_file_path,
+                'small' => $this->small,
+                'medium' => $this->medium,
+            ]
+        ];
+    }
+}
