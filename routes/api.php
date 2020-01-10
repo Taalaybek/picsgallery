d1bf166bb7aaef648becab14cd9907e0ba49a25b
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-10 23:54:21 +0600

    Registered some new routes

--- routes/api.php
+++ routes/api.php
@@ -13,52 +13,57 @@
 
 /*** AUTHENTICATION ***/
 Route::prefix('v1/auth')->group(function () {
-    Route::namespace('API\V1\Auth')->group(function () {
-        Route::post('login', 'LoginController@login')->name('auth.login');
-        Route::post('register', 'RegisterController@register')->name('auth.register');
-        Route::get('account-activation/{token}', 'AuthController@activateToken')->name('auth.account.activate');
-        Route::post('token_refresh', 'AuthController@refreshToken')->name('auth.token_refresh');
+	Route::namespace('API\V1\Auth')->group(function () {
+		Route::post('login', 'LoginController@login')->name('auth.login');
+		Route::post('register', 'RegisterController@register')->name('auth.register');
+		Route::get('account-activation/{token}', 'AuthController@activateToken')->name('auth.account.activate');
+		Route::post('token_refresh', 'AuthController@refreshToken')->name('auth.token_refresh');
 
-        // PASSWORD RESETS
-        Route::prefix('password')->group(function () {
-            Route::post('reset', 'PasswordResetController@create')->name('auth.password.reset');
-            Route::get('find/{token}', 'PasswordResetController@find')->name('auth.passport.find');
-            Route::post('create/new', 'PasswordResetController@updatePassword')->name('auth.password.update');
-        });
+		// PASSWORD RESETS
+		Route::prefix('password')->group(function () {
+			Route::post('reset', 'PasswordResetController@create')->name('auth.password.reset');
+			Route::get('find/{token}', 'PasswordResetController@find')->name('auth.passport.find');
+			Route::post('create/new', 'PasswordResetController@updatePassword')->name('auth.password.update');
+		});
 
-        Route::middleware(['auth:api', 'verified'])->group(function () {
-           Route::get('user', 'AuthController@user')->name('auth.user');
-           Route::get('logout', 'AuthController@logout')->name('auth.logout');
-        });
-    });
+		Route::middleware(['auth:api', 'verified'])->group(function () {
+			Route::get('user', 'AuthController@user')->name('auth.user');
+			Route::get('logout', 'AuthController@logout')->name('auth.logout');
+		});
+	});
 });
 
 /*** ALBUMS ***/
 Route::prefix('v1/albums')->namespace('API\V1')->group(function () {
-    /*** PROTECTED ***/
-    Route::middleware(['auth:api', 'verified'])->group(function () {
-        Route::post('', 'AlbumController@store')->name('albums.store');
-        Route::get('creatorAlbums', 'AlbumController@creatorAlbums')->name('albums.creatorAlbums');
-        Route::delete('{album}', 'AlbumController@destroy')->name('albums.destroy');
-        Route::patch('{album}', 'AlbumController@update')->name('albums.update');
-    });
+	/*** PROTECTED ***/
+	Route::middleware(['auth:api', 'verified'])->group(function () {
+		Route::post('', 'AlbumController@store')->name('albums.store');
+		Route::get('creatorAlbums', 'AlbumController@creatorAlbums')->name('albums.creatorAlbums');
+		Route::delete('{album}', 'AlbumController@destroy')->name('albums.destroy');
+		Route::patch('{album}', 'AlbumController@update')->name('albums.update');
+	});
 
-    /*** UNPROTECTED ALBUM ROUTES ***/
-    Route::get('', 'AlbumController@index')->name('albums.index');
-    Route::get('user/{user}', 'AlbumController@userAlbums')->name('albums.userAlbums');
-    Route::get('{album}', 'AlbumController@show')->name('albums.show');
-    Route::get('{album}/user/{user}', 'AlbumController@showWithUser')->name('albums.show.withUser');
+	/*** UNPROTECTED ALBUM ROUTES ***/
+	Route::get('', 'AlbumController@index')->name('albums.index');
+	Route::get('user/{user}', 'AlbumController@userAlbums')->name('albums.userAlbums');
+	Route::get('{album}', 'AlbumController@show')->name('albums.show');
+	Route::get('{album}/user/{user}', 'AlbumController@showWithUser')->name('albums.show.withUser');
+	// RELATIONSHIPS RESOURCES'S ROUTES
+	Route::get('{album}/relationship/photos', 'AlbumRelationshipsController@photos')->name('albums.relationship.photos');
+	Route::get('{album}/relationship/oldestPhoto', 'AlbumRelationshipsController@oldestPhoto')->name('albums.relationship.oldestPhoto');
 });
 
 /*** PHOTOS ***/
 Route::prefix('v1/photos')->namespace('API\V1')->group(function () {
-    /*** PROTECTED ***/
-    Route::middleware(['auth:api', 'verified'])->group(function () {
-        Route::post('{album}', 'PhotoController@store')->name('photos.store');
-        Route::delete('{photo}', 'PhotoController@destroy')->name('photos.destroy');
-    });
+	/*** PROTECTED ***/
+	Route::middleware(['auth:api', 'verified'])->group(function () {
+		Route::post('{album}', 'PhotoController@store')->name('photos.store');
+		Route::delete('{photo}', 'PhotoController@destroy')->name('photos.destroy');
+	});
 
-    /*** UNPROTECTED ROUTES ***/
-    Route::get('', 'PhotoController@index')->name('photos.index');
-    Route::get('{photo}', 'PhotoController@show')->name('photos.show');
+	/*** UNPROTECTED ROUTES ***/
+	Route::get('', 'PhotoController@index')->name('photos.index');
+	Route::get('{photo}', 'PhotoController@show')->name('photos.show');
+	// RELATIONSHIPS RESOURCES'S ROUTES
+	Route::get('{photo}/relationship/album', 'PhotoRelationshipsController@album')->name('photos.relationships.album');
 });
