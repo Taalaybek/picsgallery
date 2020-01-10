26776e91c317c4e03a0556eaf5f332d55325e31d
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-10 23:47:38 +0600

    Changed indent and size, added relationships and self link

--- app/Http/Resources/PhotoResource.php
+++ app/Http/Resources/PhotoResource.php
@@ -6,34 +6,31 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class PhotoResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return [
-            'type' => 'photos',
-            'id' => $this->id,
-            'attributes' => [
-                'name' => $this->name,
-                'base_name' => $this->base_name,
-                'full_name' => $this->full_name,
-                'mime_type' => $this->mime_type,
-                'size' => $this->size,
-                'original_file_path' => $this->original_file_path,
-                'small' => $this->small,
-                'medium' => $this->medium,
-            ]
-        ];
-    }
-
-    public function with($request)
-    {
-        return [
-            'links' => ['self' => route('photos.show', ['photo' => $this->id])]
-        ];
-    }
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function toArray($request)
+	{
+		return [
+			'type' => 'photos',
+			'id' => $this->id,
+			'attributes' => [
+				'name' => $this->name,
+				'base_name' => $this->base_name,
+				'full_name' => $this->full_name,
+				'mime_type' => $this->mime_type,
+				'size' => $this->size,
+				'original_file_path' => $this->original_file_path,
+				'small' => $this->small,
+				'medium' => $this->medium
+			],
+			'relationships' => new PhotoRelationshipResource($this),
+			'links' => [
+				'self' => route('photos.show', ['photo' => $this->id])
+			]
+		];
+	}
 }
