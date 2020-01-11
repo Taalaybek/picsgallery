6fcb62c16cc9de436094d9c30e610dee2f2113c4
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-11 18:02:20 +0600

    Handle null file name

--- app/Traits/Uploadable.php
+++ app/Traits/Uploadable.php
@@ -15,284 +15,289 @@ use Illuminate\Support\Facades\Artisan;
 
 trait Uploadable
 {
-    private $file;
-    private $path;
-    private $disk;
-    private $album;
-    private $fileName;
-    protected $data = [];
-    private $thumbnails = [];
-    private $separator = DIRECTORY_SEPARATOR;
+	private $file;
+	private $path;
+	private $disk;
+	private $album;
+	private $fileName;
+	protected $data = [];
+	private $thumbnails = [];
+	private $separator = DIRECTORY_SEPARATOR;
 
-    /**
-     * Returns edited file's name
-     *
-     * @return mixed
-     */
-    private function getFileName()
-    {
-        return auth()->user()->username . '_' .
-            Str::random(8) .
-            str_replace(' ', '_', '_' . $this->fileName);
-    }
+	/**
+	 * Returns edited file's name
+	 *
+	 * @return mixed
+	 */
+	private function getFileName()
+	{
+		return auth()->user()->username . '_' .
+			Str::random(8) .
+			str_replace(' ', '_', '_' . $this->fileName);
+	}
 
-    /**
-     * Accept UploadedFile instance
-     *
-     * @param UploadedFile $file
-     * @return $this
-     */
-    private function setFile(UploadedFile $file)
-    {
-        $this->file = $file;
-        return $this;
-    }
+	/**
+	 * Accept UploadedFile instance
+	 *
+	 * @param UploadedFile $file
+	 * @return $this
+	 */
+	private function setFile(UploadedFile $file)
+	{
+		$this->file = $file;
+		return $this;
+	}
 
-    /**
-     * Returns UploadedFile instance
-     *
-     * @return UploadedFile object
-     */
-    private function getFile()
-    {
-        return $this->file;
-    }
+	/**
+	 * Returns UploadedFile instance
+	 *
+	 * @return UploadedFile object
+	 */
+	private function getFile()
+	{
+		return $this->file;
+	}
 
-    /**
-     * Returns file's size
-     *
-     * @return string
-     */
-    private function getFileSize(): string
-    {
-        $size = round($this->getFile()->getSize() / 1024, 1);
-        switch ($size){
-            case $size < 1000;
-                $size = $size . 'Kb';
-                break;
-            case $size > 1000;
-                $size = round($size / 1024, 2) . 'Mb';
-        }
-        return $size;
-    }
+	/**
+	 * Returns file's size
+	 *
+	 * @return string
+	 */
+	private function getFileSize(): string
+	{
+		$size = round($this->getFile()->getSize() / 1024, 1);
+		switch ($size){
+			case $size < 1000;
+				$size = $size . 'Kb';
+				break;
+			case $size > 1000;
+				$size = round($size / 1024, 2) . 'Mb';
+		}
+		return $size;
+	}
 
-    /**
-     * Returns file's extension
-     *
-     * @return mixed
-     */
-    private function getExtension()
-    {
-        return $this->getFile()->extension();
-    }
+	/**
+	 * Returns file's extension
+	 *
+	 * @return mixed
+	 */
+	private function getExtension()
+	{
+		return $this->getFile()->extension();
+	}
 
-    /**
-     * Returns base name of file
-     *
-     * @return string
-     */
-    private function getBaseName(): string
-    {
-        return $this->data['base_name'];
-    }
+	/**
+	 * Returns base name of file
+	 *
+	 * @return string
+	 */
+	private function getBaseName(): string
+	{
+		return $this->data['base_name'];
+	}
 
-    /**
-     * Returns full name of file with extension
-     *
-     * @return string
-     */
-    public function getFullName(): string
-    {
-        return $this->data['full_name'];
-    }
+	/**
+	 * Returns full name of file with extension
+	 *
+	 * @return string
+	 */
+	public function getFullName(): string
+	{
+		return $this->data['full_name'];
+	}
 
-    /**
-     * Accept file's name
-     *
-     * @param string $fileName
-     * @return $this
-     */
-    public function setFileName(string $fileName)
-    {
-        $this->fileName = $fileName;
-        return $this;
-    }
+	/**
+	 * Accept file's name
+	 *
+	 * @param string $fileName
+	 * @return $this
+	 */
+	public function setFileName(string $fileName)
+	{
+		$this->fileName = $fileName;
+		return $this;
+	}
 
-    /**
-     * @param string $path
-     * @return $this
-     */
-    public function setPath(string $path)
-    {
-        $this->path = $path;
-        return $this;
-    }
+	/**
+	 * @param string $path
+	 * @return $this
+	 */
+	public function setPath(string $path)
+	{
+		$this->path = $path;
+		return $this;
+	}
 
-    /**
-     * Returns original file's path
-     *
-     * @return string
-     */
-    private function getPath()
-    {
-        return $this->path;
-    }
+	/**
+	 * Returns original file's path
+	 *
+	 * @return string
+	 */
+	private function getPath()
+	{
+		return $this->path;
+	}
 
-    /**
-     * Returns thumbnails path
-     * @return string
-     */
-    public function getThumbnailsPath()
-    {
-        return $this->disk.'/'.$this->album->id.'/'.'thumbnails';
-    }
+	/**
+	 * Returns thumbnails path
+	 * @return string
+	 */
+	public function getThumbnailsPath()
+	{
+		return $this->disk.'/'.$this->album->id.'/'.'thumbnails';
+	}
 
-    private function getThumbnailImagePath(int $width): string
-    {
-        return $this->getThumbnailsPath().'/'.$this->getBaseName().'_'.$width.'.'.$this->getExtension();
-    }
-    /**
-     * Accept file and file's name
-     *
-     * @param UploadedFile $file
-     * @param string $fileName
-     * @param Album $album
-     * @param string $disk
-     * @return Uploadable
-     */
-    public function setData(UploadedFile $file, string $fileName, Album $album, string $disk)
-    {
-        $this->data['name'] = $fileName;
-        $this->setFileName($fileName);
-        $this->setFile($file);
-        $this->album = $album;
-        $this->disk = $disk;
-        $this->data['base_name'] = $this->getFileName();
-        $this->data['full_name'] = $this->getBaseName().'.'.$this->getExtension();
-        $this->data['mime_type'] = $this->getExtension();
-        $this->data['size'] = $this->getFileSize();
+	private function getThumbnailImagePath(int $width): string
+	{
+		return $this->getThumbnailsPath().'/'.$this->getBaseName().'_'.$width.'.'.$this->getExtension();
+	}
 
-        return $this;
-    }
+	/**
+	 * Accept file and file's name
+	 *
+	 * @param UploadedFile $file
+	 * @param string $fileName
+	 * @param Album $album
+	 * @param string $disk
+	 * @return Uploadable
+	 */
+	public function setData(UploadedFile $file, Album $album, string $fileName = null, string $disk = 'albums')
+	{
+		if (!\is_null($fileName)) {
+			$this->data['name'] = $fileName;
+			$this->setFileName($fileName);
+		} else {
+			$this->setFileName(mb_strtolower(env('APP_NAME')));
+		}
 
-    /**
-     * Returns formatted data
-     *
-     * @return array
-     */
-    private function getData(): array
-    {
-        return $this->data;
-    }
+		$this->setFile($file);
+		$this->album = $album;
+		$this->disk = $disk;
+		$this->data['base_name'] = $this->getFileName();
+		$this->data['full_name'] = $this->getBaseName().'.'.$this->getExtension();
+		$this->data['mime_type'] = $this->getExtension();
+		$this->data['size'] = $this->getFileSize();
 
-    /**
-     * Uploads request file
-     *
-     * @return $this
-     */
-    public function toUpload()
-    {
-        $this->detectDiskFolder()
-            ->detectDiskSymlink();
+		return $this;
+	}
 
-        $this->setPath(Storage::putFileAs(
-            $this->disk.'/'.$this->album->id,
-            $this->getFile(),
-            $this->getFullName()
-        ));
+	/**
+	 * Returns formatted data
+	 *
+	 * @return array
+	 */
+	private function getData(): array
+	{
+		return $this->data;
+	}
 
-        $this->data['original_file_path'] = $this->getPath();
-        return $this;
-    }
+	/**
+	 * Uploads request file
+	 *
+	 * @return $this
+	 */
+	public function toUpload()
+	{
+		$this->detectDiskFolder()
+			->detectDiskSymlink();
 
-    /**
-     * Makes thumbnail
-     *
-     * @param string $size
-     * @param int|null $width
-     * @param int|null $height
-     * @return $this | JsonResponse
-     */
-    public function makeThumbnail(string $size, int $width = null, int $height = null)
-    {
-        $this->detectThumbnailsFolder();
+		$this->setPath(Storage::putFileAs(
+			$this->disk.'/'.$this->album->id,
+			$this->getFile(),
+			$this->getFullName()
+		));
 
-        try {
-            $thumb = null;
-            if (Storage::exists($this->getPath())) {
-                $thumb = Image::make(Storage::get($this->getPath()));
-            }
+		$this->data['original_file_path'] = $this->getPath();
+		return $this;
+	}
 
-            if (!is_null($thumb) && !($thumb->getWidth() <= $width)) {
-                $thumb->resize($width, $height, function ($constraint) {
-                    $constraint->aspectRatio();
-                });
+	/**
+	 * Makes thumbnail
+	 *
+	 * @param string $size
+	 * @param int|null $width
+	 * @param int|null $height
+	 * @return $this | JsonResponse
+	 */
+	public function makeThumbnail(string $size, int $width = null, int $height = null)
+	{
+		$this->detectThumbnailsFolder();
 
-                $thumb->save(
-                    Storage::path($this->getThumbnailImagePath($width)),
-                    100
-                );
+		try {
+			$thumb = null;
+			if (Storage::exists($this->getPath())) {
+				$thumb = Image::make(Storage::get($this->getPath()));
+			}
 
-                $this->thumbnails[$size] = [
-                    'width' => $width,
-                    'path' => $this->getThumbnailImagePath($width),
-                ];
+			if (!is_null($thumb) && !($thumb->getWidth() <= $width)) {
+				$thumb->resize($width, $height, function ($constraint) {
+					$constraint->aspectRatio();
+				});
 
-            }
-        } catch (FileNotFoundException $e) {
-            return response()->json(['message' => $e->getMessage()], 404);
-        }
+				$thumb->save(
+					Storage::path($this->getThumbnailImagePath($width)),
+					100
+				);
 
-        return $this;
-    }
+				$this->thumbnails[$size] = [
+					'width' => $width,
+					'path' => $this->getThumbnailImagePath($width),
+				];
+			}
+		} catch (FileNotFoundException $e) {
+			return response()->json(['message' => $e->getMessage()], 404);
+		}
 
-    private function detectDiskFolder()
-    {
-        if (!file_exists(Storage::path($this->disk))) {
-            Storage::makeDirectory($this->disk);
-        }
+		return $this;
+	}
 
-        return $this;
-    }
+	private function detectDiskFolder()
+	{
+		if (!file_exists(Storage::path($this->disk))) {
+			Storage::makeDirectory($this->disk);
+		}
 
-    /**
-     * Determines if the disk folder's symlink
-     * exists and creates it if it doesn't
-     *
-     * @return $this
-     */
-    private function detectDiskSymlink()
-    {
-        if (!file_exists(public_path(storage_path($this->disk)))) {
-            Artisan::call('directory:link '.$this->disk);
-        }
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	 * Determines if the disk folder's symlink
+	 * exists and creates it if it doesn't
+	 *
+	 * @return $this
+	 */
+	private function detectDiskSymlink()
+	{
+		if (!file_exists(public_path(storage_path($this->disk)))) {
+			Artisan::call('directory:link '.$this->disk);
+		}
 
-    /**
-     * Determines if the thumbnails exists
-     * and creates it if it doesn't.
-     *
-     * @return $this
-     */
-    private function detectThumbnailsFolder()
-    {
-        if (!file_exists(Storage::path($this->getThumbnailsPath()))) {
-            Storage::makeDirectory($this->getThumbnailsPath());
-        }
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	 * Determines if the thumbnails exists
+	 * and creates it if it doesn't.
+	 *
+	 * @return $this
+	 */
+	private function detectThumbnailsFolder()
+	{
+		if (!file_exists(Storage::path($this->getThumbnailsPath()))) {
+			Storage::makeDirectory($this->getThumbnailsPath());
+		}
 
-    /**
-     * Returns Photo object instance
-     *
-     * @return Photo
-     */
-    public function save()
-    {
-        $this->data['thumbnails'] = json_encode($this->thumbnails);
-        return new Photo($this->getData());
-    }
+		return $this;
+	}
+
+	/**
+	 * Returns Photo object instance
+	 *
+	 * @return Photo
+	 */
+	public function save()
+	{
+		$this->data['thumbnails'] = json_encode($this->thumbnails);
+		return new Photo($this->getData());
+	}
 }
