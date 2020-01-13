03f48e4541d577cf152df13623ebb753cfb8d8ef
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-14 03:02:26 +0600

    Replaced method

--- app/Http/Controllers/API/V1/AlbumRelationshipsController.php
+++ app/Http/Controllers/API/V1/AlbumRelationshipsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\API\V1;
 
+use App\Models\User;
 use App\Models\Album;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AlbumResource;
 use App\Http\Resources\PhotoResource;
 use App\Http\Resources\PhotoCollection;
 
@@ -29,5 +31,17 @@ class AlbumRelationshipsController extends Controller
 	public function oldestPhoto(Album $album): PhotoResource
 	{
 		return new PhotoResource($album->photos()->oldest()->first());
+	}
+
+	/**
+	 * Returns album with user resource
+	 *
+	 * @param Album $album
+	 * @param User $user
+	 * @return AlbumResource
+	 */
+	public function withUser(Album $album, User $user): AlbumResource
+	{
+		return new AlbumResource($user->albums()->where('id', $album->id)->first(), true);
 	}
 }
