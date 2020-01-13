fffcdb02f0c7d8c801048e84ea9b984f0f020943
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-14 02:24:48 +0600

    Deleted whitespaces, changed indent size

--- app/Http/Controllers/API/V1/AlbumController.php
+++ app/Http/Controllers/API/V1/AlbumController.php
@@ -9,123 +9,121 @@ use App\Events\AlbumDeletedEvent;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Cache;
 use App\Http\Resources\AlbumResource;
+use Illuminate\Support\Facades\Cache;
 use App\Http\Resources\AlbumsCollection;
-use App\Http\Resources\AlbumRelatedWithUserResource;
 
 class AlbumController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return AlbumsCollection
-     */
-    public function index(): AlbumsCollection
-    {
-        return new AlbumsCollection(Cache::remember('albums.index', 60, function () {
-            return DB::table('albums')->latest()->paginate(12);
-        }));
-    }
+	/**
+		* Display a listing of the resource.
+		*
+		* @return AlbumsCollection
+		*/
+	public function index(): AlbumsCollection
+	{
+		return new AlbumsCollection(Cache::remember('albums.index', 60, function () {
+			return DB::table('albums')->latest()->paginate(12);
+		}));
+	}
 
-    /**
-     * Returns albums of the current user
-     * @return AlbumsCollection
-     */
-    public function creatorAlbums(): AlbumsCollection
-    {
-        return new AlbumsCollection(auth()->user()->albums()->paginate(12));
-    }
+	/**
+		* Returns albums of the current user
+		* @return AlbumsCollection
+		*/
+	public function creatorAlbums(): AlbumsCollection
+	{
+		return new AlbumsCollection(auth()->user()->albums()->latest()->paginate(12));
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return AlbumResource
-     */
-    public function store(Request $request): AlbumResource
-    {
-        $request->validate([
-            'name' => 'required|string|min:6|max:255',
-            'description' => 'sometimes|string|min:10|max:400'
-        ]);
-        $request->merge(['creator_id' => auth()->user()->id]);
+	/**
+		* Store a newly created resource in storage.
+		*
+		* @param Request $request
+		* @return AlbumResource
+		*/
+	public function store(Request $request): AlbumResource
+	{
+		$request->validate([
+				'name' => 'required|string|min:6|max:255',
+				'description' => 'sometimes|string|min:10|max:400'
+		]);
 
-        $album = Album::create($request->all());
+		$request->merge(['creator_id' => auth()->user()->id]);
 
-        return new AlbumResource($album);
-    }
+		$album = Album::create($request->all());
 
-    /**
-     * Display the specified resource.
-     *
-     * @param Album $album
-     * @return AlbumResource
-     */
-    public function show(Album $album)
-    {
-        return new AlbumResource($album);
-    }
+		return new AlbumResource($album);
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param Album $album
-     * @return AlbumResource
-     */
-    public function update(Request $request, Album $album)
-    {
-        if (auth()->user()->can('update', $album)) {
-            $request->validate([
-                'name' => 'sometimes|string|min:6|max:255',
-                'description' => 'sometimes|string|min:10|max:400'
-            ]);
+	/**
+		* Display the specified resource.
+		*
+		* @param Album $album
+		* @return AlbumResource
+		*/
+	public function show(Album $album)
+	{
+		return new AlbumResource($album);
+	}
 
-            $album->update($request->all());
+	/**
+		* Update the specified resource in storage.
+		*
+		* @param Request $request
+		* @param Album $album
+		* @return AlbumResource
+		*/
+	public function update(Request $request, Album $album)
+	{
+		if (auth()->user()->can('update', $album)) {
+			$request->validate([
+				'name' => 'sometimes|string|min:6|max:255',
+				'description' => 'sometimes|string|min:10|max:400'
+			]);
 
-            return new AlbumResource($album);
-        }
+			$album->update($request->all());
 
-        return response()->json(['message' => 'You need access to do this action'], 401);
-    }
+			return new AlbumResource($album);
+		}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param Album $album
-     * @return JsonResponse
-     * @throws \Exception
-     */
-    public function destroy(Album $album)
-    {
-        if (auth()->user()->can('delete', $album)) {
-            $album->delete();
-            event(new AlbumDeletedEvent());
-            return response()->json(['message' => 'Successfully deleted the album'], 200);
-        }
+		return response()->json(['message' => 'You need access to do this action'], 401);
+	}
 
-        return \response()->json(['message' => 'You need access to do this action'], 401);
-    }
+	/**
+		* Remove the specified resource from storage.
+		*
+		* @param Album $album
+		* @return JsonResponse
+		* @throws \Exception
+		*/
+	public function destroy(Album $album)
+	{
+		if (auth()->user()->can('delete', $album)) {
+			$album->delete();
+			event(new AlbumDeletedEvent());
+			return response()->json(['message' => 'Successfully deleted the album'], 200);
+		}
 
-    /**
-     * @param Album $album
-     * @param User $user
-     * @return AlbumRelatedWithUserResource
-     */
-    public function showWithUser(Album $album, User $user): AlbumRelatedWithUserResource
-    {
-        $album = Album::where(['creator_id' => $user->id, 'id' => $album->id])->first();
+		return \response()->json(['message' => 'You need access to do this action'], 401);
+	}
 
-        return new AlbumRelatedWithUserResource($album);
-    }
+	/**
+		* @param Album $album
+		* @param User $user
+		* @return AlbumResource
+		*/
+	public function showWithUser(Album $album, User $user): AlbumResource
+	{
+		return new AlbumResource($user->albums()->where('id', $album->id)->first(), true);
+	}
 
-    /**
-     * @param User $user
-     * @return AlbumsCollection
-     */
-    public function userAlbums(User $user): AlbumsCollection
-    {
-        return new AlbumsCollection($user->albums()->paginate(12));
-    }
+	/**
+		* @param User $user
+		* @return AlbumsCollection
+		*/
+	public function userAlbums(User $user): AlbumsCollection
+	{
+		return new AlbumsCollection($user->albums()->paginate(12));
+	}
 }
