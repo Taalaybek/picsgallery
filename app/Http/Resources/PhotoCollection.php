922a2c5d7aa5e69857fb47677ed4df12124b2be7
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-17 00:46:58 +0600

    Made PhotoCollection managable

--- app/Http/Resources/PhotoCollection.php
+++ app/Http/Resources/PhotoCollection.php
@@ -6,21 +6,38 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class PhotoCollection extends ResourceCollection
 {
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
-     */
-    public function toArray($request)
-    {
-        return PhotoResource::collection($this->collection);
-    }
+	protected $resourceInstance;
 
-    public function with($request)
-    {
-        return [
-            'links' => ['self' => route('photos.index')]
-        ];
-    }
+	public function __construct($resource, $resourceInstance = null)
+	{
+		parent::__construct($resource);
+		$this->resourceInstance = $resourceInstance;
+	}
+
+	/**
+	 * Transform the resource collection into an array.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+	 */
+	public function toArray($request)
+	{
+		if (!is_null($this->resourceInstance) &&
+		  class_exists($this->resourceInstance) && 
+			array_key_first(class_parents($this->resourceInstance)) == 'Illuminate\Http\Resources\Json\JsonResource'
+		) {
+		  return $this->resourceInstance::collection($this->collection);
+		}
+
+		if (is_null($this->resourceInstance)) {
+			return PhotoResource::collection($this->collection);
+		}
+	}
+
+	public function with($request)
+	{
+		return [
+			'links' => ['self' => route('photos.index')]
+		];
+	}
 }
