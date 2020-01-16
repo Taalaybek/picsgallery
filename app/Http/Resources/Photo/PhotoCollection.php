e12f419300522dff0cb24fa0d3933b4b92f6c22e
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2020-01-17 04:10:02 +0600

    Replaced Photo entity resource into App\Resources\Photo folder

--- app/Http/Resources/Photo/PhotoCollection.php
+++ app/Http/Resources/Photo/PhotoCollection.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Resources;
+namespace App\Http\Resources\Photo;
 
+use App\Http\Resources\Photo\PhotoResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class PhotoCollection extends ResourceCollection
